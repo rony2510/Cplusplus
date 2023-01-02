604ef8c3b36a4d170e3f36907d25455c8f7ed30a
rony2510 <ronyhajong2000@gmail.com>
2023-01-03 02:18:32 +0600

    Logical operator

---
+++ Operator/Logical/not.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    bool b=true;
+
+    bool ok=!b;
+    
+    cout<<ok<<endl;
+
+
+    return 0;
+}
---
+++ Operator/Logical/or.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x,y;
+    cin>>x>>y;
+
+    bool b=(x>5 || y>10);
+
+    cout<<b<<endl;
+
+
+    return 0;
+}
